1a38f111da39448e8a96e06bd6383f58caa1738b
El Taha, Nadim <nadim.el.taha@i2cat.net>
2014-02-12 09:20:32 +0100

    TVRING-195: Adds getters&setters of segdur, framerate and samplrate

--- i2libdash/i2libdash.c
+++ i2libdash/i2libdash.c
@@ -6,5 +6,26 @@ uint32_t h264_handler(byte *input_data, uint32_t size_input, i2ctx *context);
 // Privada. Función te dice si un sample es intra o no.
 uint8_t is_key_frame(byte *input_data, uint32_t size_input);
 
+void set_segment_duration(uint32_t segment_duration, i2ctx *context){
+	context->duration_ms = segment_duration;
+}
 
+uint32_t get_segment_duration(i2ctx *context){
+	return context->duration_ms;
+}
 
+void set_frame_rate(uint32_t frame_rate, i2ctx_video *ctxVideo){
+	ctxVideo->frame_rate = frame_rate;
+}
+
+uint32_t get_frame_rate(i2ctx_video *ctxVideo){
+	return ctxVideo->frame_rate;
+}
+
+void set_sample_rate(uint32_t sample_rate, i2ctx_audio *ctxAudio){
+	ctxAudio->sample_rate = sample_rate;
+}
+
+uint32_t get_sample_rate(i2ctx_audio *ctxAudio){
+	return ctxAudio->sample_rate;
+}
--- i2libdash/i2libdash.h
+++ i2libdash/i2libdash.h
@@ -14,6 +14,18 @@ uint32_t init_video_handler(byte *input_data, uint32_t size_input, byte *output_
 uint32_t init_audio_handler(byte *input_data, uint32_t size_input, byte *output_data, i2ctx *context);
 // Función que reserva memoria para el context
 uint32_t context_initializer(i2ctx *context);
+// Setter de duración de segmento
+void set_segment_duration(uint32_t segment_duration, i2ctx *context);
+// Getter de duración de segmento
+uint32_t get_segment_duration(i2ctx *context);
+// Setter de frame rate para vídeo
+void set_frame_rate(uint32_t frame_rate, i2ctx_video *ctxVideo);
+// Getter de frame rate para vídeo
+uint32_t get_frame_rate(i2ctx_video *ctxVideo);
+// Setter de sample rate para audio
+void set_sample_rate(uint32_t sample_rate, i2ctx_audio *ctxAudio);
+// Getter de sample rate para audio
+uint32_t get_sample_rate(i2ctx_audio *ctxAudio);
 
 #endif
 
