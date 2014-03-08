f7226408160dfdff58addac2ae91c5b3bdd79eab
El Taha, Nadim <nadim.el.taha@i2cat.net>
2014-03-08 21:01:00 +0100

    TVRING_220: Implements Audio HE-AAC segmentation for RTP Streams

--- test/lib_test_audio.c
+++ test/lib_test_audio.c
@@ -7,44 +7,43 @@
 int main(int argc, char *argv[]){
 	i2ctx *context;
 	byte *metadata = NULL;
-	byte *destination_data = NULL, *source_data = NULL;
-	uint32_t metadata_size = 0, size_source_data = 0, initial_timestamp = 0;
+	byte *destination_data = NULL;
+	uint32_t metadata_size = 0, initial_timestamp = 0, message_length = 0;
 	uint32_t duration_sample = 0, timestamp = 0, previous_timestamp = 0, i = 0, seg_size = 0;
-	uint32_t sample_count = 0, segment_count = 0, init_audio = 0;
-<<<<<<< HEAD
-=======
-	uint16_t aac_length_header = 0, ntohs_aac_length_header = 0, count = 0;
->>>>>>> TVRING-192: Obtains AU Header length
-	uint8_t i2error = 0, is_intra = 1;
+	uint32_t segment_count = 0, init_audio = 0;
+	uint16_t segment_size = 0, sample_size_shifted = 0;
+	uint16_t hton_aac_length = 0, hton_sample_size = 0;
+	uint8_t i2error = 0;
 	struct sockaddr_in  sock_addr_input;
     socklen_t sockaddrlen = sizeof(struct sockaddr);
     int sock_origen;
-    uint32_t c = 0, hton_timestamp = 0, decode_time = 0, aac_length = 0;
+    uint32_t hton_timestamp = 0, decode_time = 0, aac_length = 0, sample_size = 0;
+    uint32_t sample_pos = 0, au_num = 0;
 	byte buffer_in[3000];
 	struct timeval to;
 	fd_set ready;
 	FILE *output_audio_i;
 	float duration_sample_f = 0.00, decode_time_f = 0.00;
 	char* representation = "480";
+
 	if (argc == 2)
 		representation = argv[argc - 1];
 
-	source_data = (byte *) malloc(MAX_DAT);
     sock_origen = socket(AF_INET, SOCK_DGRAM, 0 );
     if(sock_origen == -1) {
 		perror("Error socket\n");
 		exit(-1);
-	} //endif
+	}
 
     bzero((char *)&sock_addr_input, sizeof(sock_addr_input) );
     sock_addr_input.sin_family = AF_INET;
     sock_addr_input.sin_port = htons(10002);
 
-if (bind(sock_origen, (struct sockaddr *)&sock_addr_input, sizeof(sock_addr_input) ) == -1) {
+	if (bind(sock_origen, (struct sockaddr *)&sock_addr_input, sizeof(sock_addr_input) ) == -1) {
 		perror("Error bind\n");
 		close(sock_origen);
 		exit(-1);
-	} //endif
+	}
 	i2error= context_initializer(&context, AUDIO_TYPE);
 	if (i2error == I2ERROR_MEDIA_TYPE) {
 		printf ("I2ERROR_MEDIA_TYPE! code %d\n", i2error);
@@ -66,7 +65,6 @@ if (bind(sock_origen, (struct sockaddr *)&sock_addr_input, sizeof(sock_addr_inpu
 
 
     while (1) {
-		//int j=0;
 		FD_ZERO(&ready); 
 		FD_SET(sock_origen, &ready);
 		to.tv_sec = 5;
@@ -77,93 +75,96 @@ if (bind(sock_origen, (struct sockaddr *)&sock_addr_input, sizeof(sock_addr_inpu
 		}
 		if (FD_ISSET(sock_origen, &ready)) {
 
-			c = recvfrom (sock_origen, buffer_in, sizeof(buffer_in), 0, (struct sockaddr *)&sock_addr_input, &sockaddrlen);
-			memcpy(&hton_timestamp, buffer_in+4, sizeof(hton_timestamp));
+			// Obtain the message length and buffer_in
+			message_length = recvfrom (sock_origen, buffer_in, sizeof(buffer_in), 0, (struct sockaddr *)&sock_addr_input, &sockaddrlen);
 
+			// Obtain timestamp
+			memcpy(&hton_timestamp, buffer_in+4, sizeof(hton_timestamp));
 			timestamp = htonl(hton_timestamp);
 
-			memcpy(&aac_length_header, buffer_in + RTP_LENGTH_HEADER, sizeof(aac_length_header));
-			printf("aac_length_header: %u\n", aac_length_header);
-			ntohs_aac_length_header = ntohs(aac_length_header) / (AU_LENGTH_HEADER*BYTE_SIZE);
-			printf("ntohs_aac_length_header: %u\n", ntohs_aac_length_header);
+			// Obtain AAC stuff
+			memcpy(&hton_aac_length, buffer_in+RTP_LENGTH_HEADER, sizeof(hton_aac_length));
+			aac_length = ntohs(hton_aac_length);
 
-			count = ntohs_aac_length_header;
+			au_num = aac_length / (AU_LENGTH_HEADER * BYTE_SIZE);
 
-			if (i == 0) {
+			// First RTP Datagram
+			if(i == 0)  {
+				decode_time = 0;
 				previous_timestamp = timestamp;
 				initial_timestamp = timestamp;
-				decode_time = 0;
-				size_source_data = 0;
+				// TODO put init generation here
 				init_audio = init_audio_handler(metadata, metadata_size, destination_data, &context);
 				if (init_audio > I2ERROR_MAX) {
 					char path[250];
 					bzero(path, 250);
 					sprintf(path, "%s%s%s","/tmp/pruebas/i2lib/i2libtest_", representation, "_audio_init.m4a");
 					output_audio_i = fopen(path, "w");
-					int i = 0;
-					// int fputc(int c, FILE *stream);
-					for(i = 0; i < init_audio; i++) {
-						fputc(destination_data[i], output_audio_i);
+					int k = 0;
+					for(k = 0; k < init_audio; k++) {
+						fputc(destination_data[k], output_audio_i);
 					}
 					fclose(output_audio_i);
 					printf("INIT i2libtest_%s_audio_init.m4a done\n", representation);
 				}
 			}
-
-			if (previous_timestamp != timestamp) {
-				sample_count++;
-				duration_sample = ((timestamp - previous_timestamp) / AAC_48K_FREQUENCY_MS);
+			// New RTP Datagram
+			if(previous_timestamp != timestamp) {
 				decode_time = ((previous_timestamp - initial_timestamp) / AAC_48K_FREQUENCY_MS);
-				duration_sample_f+= (((float)(timestamp - previous_timestamp))/((float) (AAC_48K_FREQUENCY_MS)));
-				duration_sample_f-= (float) duration_sample;
-				if (duration_sample_f >= 1) {
-					//printf("ES MAYOR QUE UNO!!!!!!!!!!!!!!!!!!!!!!!\n");
-					duration_sample++;
-					duration_sample_f--;
-				}
 				decode_time_f= (((float)(previous_timestamp - initial_timestamp))/((float) (AAC_48K_FREQUENCY_MS)));
 				decode_time_f-= (float) decode_time;
-				if ((decode_time_f >= 0.5) ) {
-					//printf("INCREMENTO DECODE\n");
+				if ((decode_time_f >= 0.5) )
 					decode_time++;
-					//decode_time_f--;
+			}
+
+			// TODO check in the first segment, maybe wrong
+			duration_sample = ((timestamp - previous_timestamp) / AAC_48K_FREQUENCY_MS);
+
+			duration_sample_f+= (((float)(timestamp - previous_timestamp))/((float) (AAC_48K_FREQUENCY_MS)));
+
+			duration_sample_f-= (float) duration_sample;
+
+			if (duration_sample_f >= 1) {
+				duration_sample++;
+				duration_sample_f--;
+			}
+
+			previous_timestamp = timestamp;
+
+			// Write samples
+			sample_pos = RTP_LENGTH_HEADER + (AU_LENGTH_HEADER*(au_num+1));
+
+			int j = 0;
+			for(j = 0; j < au_num; j++)  {
+				memcpy(&hton_sample_size, buffer_in+RTP_LENGTH_HEADER+(AU_LENGTH_HEADER + (AU_LENGTH_HEADER*j)), sizeof(hton_sample_size));
+				sample_size = ntohs(hton_sample_size);
+				
+				sample_size_shifted = (sample_size>>=3);
+				printf("\nSAMPLE_SIZE %d: %u", j,sample_size_shifted);
+
+				segment_size = add_sample(buffer_in + sample_pos, sample_size_shifted, duration_sample, decode_time, AUDIO_TYPE, destination_data, 1, &context);
+
+				sample_pos += sample_size_shifted;
+			}
+
+			printf("\nTIMESTAMP: %u\nAAC_LENGTH: %u\nAU_NUM: %u\nSAMPLE_POS: %u\nSAMPLE_SIZE: %u\nMESSAGE_LENGTH: %u\n", timestamp, aac_length, au_num, sample_pos, sample_size_shifted, message_length);
+
+			if(segment_size > I2ERROR_MAX)  {
+				char path[250];
+				bzero(path, 250);
+				printf("\nSEGMENT i2libtest_%s_audio_%d_1.m4a done\n", representation, segment_count);
+				sprintf(path, "%s%s%s%d%s","/tmp/pruebas/i2lib/i2libtest_", representation, "_audio_", segment_count, "_1.m4a");
+				segment_count++;
+				duration_sample_f = 0.00;
+				output_audio_i = fopen(path, "w");
+				int j = 0;
+				for(j = 0; j < segment_size; j++) {
+					fputc(destination_data[j], output_audio_i);
 				}
-				//printf("before add samples\n");
-				seg_size = add_sample(source_data, size_source_data, duration_sample, decode_time, AUDIO_TYPE, destination_data, is_intra, &context);
-				//printf("after add samples\n");
-				//is_intra = 0;
-				if (seg_size > I2ERROR_MAX) {
-					//printf("initial time: %u previous time %u timestamp %u\ndecode time %u resta %u\n",  initial_timestamp, previous_timestamp, timestamp, decode_time, (previous_timestamp - initial_timestamp));
-					char path[250];
-					bzero(path, 250);
-					printf("\nSEGMENT i2libtest_%s_audio_%d_1.m4a done\n", representation, segment_count);
-					sprintf(path, "%s%s%s%d%s","/tmp/pruebas/i2lib/i2libtest_", representation, "_audio_", segment_count, "_1.m4a");
-					//printf("After path: %s\n", path);
-					segment_count++;
-					sample_count = 0;
-					duration_sample_f = 0.00;
-					//printf("OK VIDEO SEGMENT! %d\ninitial time: %u previous time %u\ndecode time %u\n", seg_size,  initial_timestamp, previous_timestamp, (((previous_timestamp - initial_timestamp) )/H264_FREQUENCY_MS));
-					//printf("Before fopen file\n");
-					output_audio_i = fopen(path, "w");
-					//printf("After open file\n");
-					int j = 0;
-					// int fputc(int c, FILE *stream);
-					//printf("Before write file\n");
-					for(j = 0; j < seg_size; j++) {
-						fputc(destination_data[j], output_audio_i);
-					}
-					fclose(output_audio_i);
-					//printf("FILE CLOSE\n");
-				}
-				aac_length = c - RTP_LENGTH_HEADER - AAC_LENGTH_HEADER;
-				memcpy(source_data + size_source_data, buffer_in + RTP_LENGTH_HEADER + AAC_LENGTH_HEADER, aac_length);
-				size_source_data= aac_length;
-			} /*else {//Same time
-				aac_length = c - RTP_LENGTH_HEADER - AAC_LENGTH_HEADER;
-				memcpy(source_data + size_source_data, buffer_in + RTP_LENGTH_HEADER + AAC_LENGTH_HEADER, aac_length);
-				size_source_data+= aac_length;
-			}*/
-		} else { /** Caso termina tx y espera 5 segundos **/
+				fclose(output_audio_i);
+				printf("FILE CLOSE\n");	
+			}
+		} else {	// wait 5 seconds until finish segment
 			printf("5 sec %u\n", context->ctxaudio->segment_data_size);
 			seg_size = finish_segment(AUDIO_TYPE, destination_data, &context);
 			if (seg_size > I2ERROR_MAX) {
@@ -173,7 +174,6 @@ if (bind(sock_origen, (struct sockaddr *)&sock_addr_input, sizeof(sock_addr_inpu
 				sprintf(path, "%s%s%s%d%s","/tmp/pruebas/i2lib/i2libtest_", representation, "_audio_", segment_count, "_1.m4a");
 				output_audio_i = fopen(path, "w");
 				int j = 0;
-				// int fputc(int c, FILE *stream);
 				for(j = 0; j < seg_size; j++) {
 					fputc(destination_data[j], output_audio_i);
 				}
