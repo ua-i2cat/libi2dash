ed9e95e0dbeb96d443c336e3339d9a68c9cbaefc
Martin German <martin.german@i2cat.net>
2014-10-06 18:01:37 +0200

    nal parser added

--- src/i2nalparser.c
+++ src/i2nalparser.c
@@ -237,35 +237,134 @@ bool spsToRbsp(unsigned char* spsHeader, uint32_t spsLength, nalHeader* headers)
 				index = (i % 8);
 				switch (index) {
 				case 0:
-					headers->sps.nalbitRateValueMinus1 = 
-					uint32_t nalbitRateValueMinus1[MAXCPBCNTMINUSONE]; //ue(v)
-					uint32_t nalcpbSizeValueMinus1[MAXCPBCNTMINUSONE]; //ue(v)
-					byte nalcbrFlag; //u(1)					
+					headers->sps.nalbitRateValueMinus1[i] = ((spsHeader[offset] & FBITRATEVALUEMINUS1H0) * 16777216) + ((spsHeader[offset+1] & FBITRATEVALUEMINUS1M) * 65536) + ((spsHeader[offset+2] & FBITRATEVALUEMINUS1M) * 256) + ((spsHeader[offset+3] & FBITRATEVALUEMINUS1L0));
+					offset+=4;
+					headers->sps.nalcpbSizeValueMinus1[i] = ((spsHeader[offset] & FCPBSIZEVALUEMINUS1H0) * 16777216) + ((spsHeader[offset+1] & FCPBSIZEVALUEMINUS1M) * 65536) + ((spsHeader[offset+2] & FCPBSIZEVALUEMINUS1M) * 256) + ((spsHeader[offset+3] & FCPBSIZEVALUEMINUS1L0));
+					offset+=4;
+					headers->sps.nalcbrFlag = (spsHeader[offset] & FCBRFLAG0) >> 7;
 					break;
 				case 1:
+					headers->sps.nalbitRateValueMinus1[i] = ((spsHeader[offset] & FBITRATEVALUEMINUS1H1) * 33554432) + ((spsHeader[offset+1] & FBITRATEVALUEMINUS1M) * 131072) + ((spsHeader[offset+2] & FBITRATEVALUEMINUS1M) * 512) + ((spsHeader[offset+3] & FBITRATEVALUEMINUS1M) * 2) + ((spsHeader[offset+4] & FBITRATEVALUEMINUS1L1) >> 7);
+					offset+=4;
+					headers->sps.nalcpbSizeValueMinus1[i] = ((spsHeader[offset] & FCPBSIZEVALUEMINUS1H1) * 33554432) + ((spsHeader[offset+1] & FCPBSIZEVALUEMINUS1M) * 131072) + ((spsHeader[offset+2] & FCPBSIZEVALUEMINUS1M) * 512) + ((spsHeader[offset+3] & FCPBSIZEVALUEMINUS1M) * 2) + ((spsHeader[offset+4] & FCPBSIZEVALUEMINUS1L1) >> 7);
+					offset+=4;
+					headers->sps.nalcbrFlag = (spsHeader[offset] & FCBRFLAG1) >> 6;
 					break;
 				case 2:
+					headers->sps.nalbitRateValueMinus1[i] = ((spsHeader[offset] & FBITRATEVALUEMINUS1H2) * 67108864) + ((spsHeader[offset+1] & FBITRATEVALUEMINUS1M) * 262144) + ((spsHeader[offset+2] & FBITRATEVALUEMINUS1M) * 1024) + ((spsHeader[offset+3] & FBITRATEVALUEMINUS1M) * 4) + ((spsHeader[offset+4] & FBITRATEVALUEMINUS1L2) >> 6);
+					offset+=4;
+					headers->sps.nalcpbSizeValueMinus1[i] = ((spsHeader[offset] & FCPBSIZEVALUEMINUS1H2) * 67108864) + ((spsHeader[offset+1] & FCPBSIZEVALUEMINUS1M) * 262144) + ((spsHeader[offset+2] & FCPBSIZEVALUEMINUS1M) * 1024) + ((spsHeader[offset+3] & FCPBSIZEVALUEMINUS1M) * 4) + ((spsHeader[offset+4] & FCPBSIZEVALUEMINUS1L2) >> 6);
+					offset+=4;
+					headers->sps.nalcbrFlag = (spsHeader[offset] & FCBRFLAG2) >> 5;
 					break;
 				case 3:
+					headers->sps.nalbitRateValueMinus1[i] = ((spsHeader[offset] & FBITRATEVALUEMINUS1H3) * 134217728) + ((spsHeader[offset+1] & FBITRATEVALUEMINUS1M) * 524288) + ((spsHeader[offset+2] & FBITRATEVALUEMINUS1M) * 2048) + ((spsHeader[offset+3] & FBITRATEVALUEMINUS1M) * 8) + ((spsHeader[offset+4] & FBITRATEVALUEMINUS1L3) >> 5);
+					offset+=4;
+					headers->sps.nalcpbSizeValueMinus1[i] = ((spsHeader[offset] & FCPBSIZEVALUEMINUS1H3) * 134217728) + ((spsHeader[offset+1] & FCPBSIZEVALUEMINUS1M) * 524288) + ((spsHeader[offset+2] & FCPBSIZEVALUEMINUS1M) * 2048) + ((spsHeader[offset+3] & FCPBSIZEVALUEMINUS1M) * 8) + ((spsHeader[offset+4] & FCPBSIZEVALUEMINUS1L3) >> 5);
+					offset+=4;
+					headers->sps.nalcbrFlag = (spsHeader[offset] & FCBRFLAG3) >> 4;
 					break;
 				case 4:
+					headers->sps.nalbitRateValueMinus1[i] = ((spsHeader[offset] & FBITRATEVALUEMINUS1H4) * 268435456) + ((spsHeader[offset+1] & FBITRATEVALUEMINUS1M) * 1048576) + ((spsHeader[offset+2] & FBITRATEVALUEMINUS1M) * 4096) + ((spsHeader[offset+3] & FBITRATEVALUEMINUS1M) * 16) + ((spsHeader[offset+4] & FBITRATEVALUEMINUS1L4) >> 4);
+					offset+=4;
+					headers->sps.nalcpbSizeValueMinus1[i] = ((spsHeader[offset] & FCPBSIZEVALUEMINUS1H4) * 268435456) + ((spsHeader[offset+1] & FCPBSIZEVALUEMINUS1M) * 1048576) + ((spsHeader[offset+2] & FCPBSIZEVALUEMINUS1M) * 4096) + ((spsHeader[offset+3] & FCPBSIZEVALUEMINUS1M) * 16) + ((spsHeader[offset+4] & FCPBSIZEVALUEMINUS1L4) >> 4);
+					offset+=4;
+					headers->sps.nalcbrFlag = (spsHeader[offset] & FCBRFLAG4) >> 3;
 					break;
 				case 5:
+					headers->sps.nalbitRateValueMinus1[i] = ((spsHeader[offset] & FBITRATEVALUEMINUS1H5) * 536870912) + ((spsHeader[offset+1] & FBITRATEVALUEMINUS1M) * 2097152) + ((spsHeader[offset+2] & FBITRATEVALUEMINUS1M) * 8192) + ((spsHeader[offset+3] & FBITRATEVALUEMINUS1M) * 32) + ((spsHeader[offset+4] & FBITRATEVALUEMINUS1L5) >> 3);
+					offset+=4;
+					headers->sps.nalcpbSizeValueMinus1[i] = ((spsHeader[offset] & FCPBSIZEVALUEMINUS1H5) * 536870912) + ((spsHeader[offset+1] & FCPBSIZEVALUEMINUS1M) * 2097152) + ((spsHeader[offset+2] & FCPBSIZEVALUEMINUS1M) * 8192) + ((spsHeader[offset+3] & FCPBSIZEVALUEMINUS1M) * 32) + ((spsHeader[offset+4] & FCPBSIZEVALUEMINUS1L5) >> 3);
+					offset+=4;
+					headers->sps.nalcbrFlag = (spsHeader[offset] & FCBRFLAG5) >> 2;
 					break;
 				case 6:
+					headers->sps.nalbitRateValueMinus1[i] = ((spsHeader[offset] & FBITRATEVALUEMINUS1H5) * 1073741824) + ((spsHeader[offset+1] & FBITRATEVALUEMINUS1M) * 2097152) + ((spsHeader[offset+2] & FBITRATEVALUEMINUS1M) * 16384) + ((spsHeader[offset+3] & FBITRATEVALUEMINUS1M) * 64) + ((spsHeader[offset+4] & FBITRATEVALUEMINUS1L5) >> 2);
+					offset+=4;
+					headers->sps.nalcpbSizeValueMinus1[i] = ((spsHeader[offset] & FCPBSIZEVALUEMINUS1H5) * 1073741824) + ((spsHeader[offset+1] & FCPBSIZEVALUEMINUS1M) * 2097152) + ((spsHeader[offset+2] & FCPBSIZEVALUEMINUS1M) * 16384) + ((spsHeader[offset+3] & FCPBSIZEVALUEMINUS1M) * 64) + ((spsHeader[offset+4] & FCPBSIZEVALUEMINUS1L5) >> 2);
+					offset+=4;
+					headers->sps.nalcbrFlag = (spsHeader[offset] & FCBRFLAG6) >> 1;
 					break;
 				case 7:
+					headers->sps.nalbitRateValueMinus1[i] = ((spsHeader[offset] & FBITRATEVALUEMINUS1H5) * 2147483648) + ((spsHeader[offset+1] & FBITRATEVALUEMINUS1M) * 4194304) + ((spsHeader[offset+2] & FBITRATEVALUEMINUS1M) * 32768) + ((spsHeader[offset+3] & FBITRATEVALUEMINUS1M) * 128) + ((spsHeader[offset+4] & FBITRATEVALUEMINUS1L5) >> 1);
+					offset+=4;
+					headers->sps.nalcpbSizeValueMinus1[i] = ((spsHeader[offset] & FCPBSIZEVALUEMINUS1H5) * 2147483648) + ((spsHeader[offset+1] & FCPBSIZEVALUEMINUS1M) * 4194304) + ((spsHeader[offset+2] & FCPBSIZEVALUEMINUS1M) * 32768) + ((spsHeader[offset+3] & FCPBSIZEVALUEMINUS1M) * 128) + ((spsHeader[offset+4] & FCPBSIZEVALUEMINUS1L5) >> 1);
+					offset+=4;
+					headers->sps.nalcbrFlag = (spsHeader[offset] & FCBRFLAG7);
+					offset++;
 					break;
 				}
-
 			}
 
-
-
-			byte nalinitialCpbRemovalDelayLengthMinus1; //u(5)
-			byte nalcpbRemovalDelayLengthMinus1; //u(5)
-			byte naldpbOutputDelayLengthMinus1; //u(5)
-			byte naltimeOffsetLength; //u(5)
+			switch (index) {
+			case 0:
+				headers->sps.nalinitialCpbRemovalDelayLengthMinus1 = (spsHeader[offset] & FINITIALCPBREMOVALDELAYLENGTHMINUS10) >> 3; //u(5)
+				headers->sps.nalcpbRemovalDelayLengthMinus1 = ((spsHeader[offset] & FCPBREMOVALDELAYLENGTHMINUS15H) << 2) | ((spsHeader[offset+1] & FCPBREMOVALDELAYLENGTHMINUS15L) >> 6); //u(5)
+				offset++;
+				headers->sps.naldpbOutputDelayLengthMinus1 = (spsHeader[offset] & FDPBOUTPUTDEALYLENGTHMINUS12) >> 1; //u(5)
+				headers->sps.naltimeOffsetLength = ((spsHeader[offset] & FTIMEOFFSETLENGT7H) << 4) | ((spsHeader[offset+1] & FTIMEOFFSETLENGTH7L) >> 4); //u(5)
+				offset++;
+				break;
+			case 1:
+				headers->sps.nalinitialCpbRemovalDelayLengthMinus1 = (spsHeader[offset] & FINITIALCPBREMOVALDELAYLENGTHMINUS11) >> 2; //u(5)
+				headers->sps.nalcpbRemovalDelayLengthMinus1 = ((spsHeader[offset] & FCPBREMOVALDELAYLENGTHMINUS16H) << 3) | ((spsHeader[offset+1] & FCPBREMOVALDELAYLENGTHMINUS16L) >> 5); //u(5)
+				offset++;
+				headers->sps.naldpbOutputDelayLengthMinus1 = (spsHeader[offset] & FDPBOUTPUTDEALYLENGTHMINUS13); //u(5)
+				offset++;
+				headers->sps.naltimeOffsetLength = (spsHeader[offset] & FTIMEOFFSETLENGTH0) >> 3; //u(5)
+				break;
+			case 2:
+				headers->sps.nalinitialCpbRemovalDelayLengthMinus1 = (spsHeader[offset] & FINITIALCPBREMOVALDELAYLENGTHMINUS12) >> 1; //u(5)
+				headers->sps.nalcpbRemovalDelayLengthMinus1 = ((spsHeader[offset] & FCPBREMOVALDELAYLENGTHMINUS17H) << 4) | ((spsHeader[offset+1] & FCPBREMOVALDELAYLENGTHMINUS17L) >> 4); //u(5)
+				offset++;
+				headers->sps.naldpbOutputDelayLengthMinus1 = ((spsHeader[offset] & FDPBOUTPUTDEALYLENGTHMINUS14H) << 1) | ((spsHeader[offset+1] & FDPBOUTPUTDEALYLENGTHMINUS14L) >> 7); //u(5)
+				offset++;
+				headers->sps.naltimeOffsetLength = (spsHeader[offset] & FTIMEOFFSETLENGTH1) >> 2; //u(5)
+				break;
+			case 3:
+				headers->sps.nalinitialCpbRemovalDelayLengthMinus1 = (spsHeader[offset] & FINITIALCPBREMOVALDELAYLENGTHMINUS13); //u(5)
+				offset++;
+				headers->sps.nalcpbRemovalDelayLengthMinus1 = ((spsHeader[offset] & FCPBREMOVALDELAYLENGTHMINUS10) >> 3); //u(5)
+				headers->sps.naldpbOutputDelayLengthMinus1 = ((spsHeader[offset] & FDPBOUTPUTDEALYLENGTHMINUS15H) << 2) | ((spsHeader[offset+1] & FDPBOUTPUTDEALYLENGTHMINUS15L) >> 6); //u(5)
+				offset++;
+				headers->sps.naltimeOffsetLength = (spsHeader[offset] & FTIMEOFFSETLENGTH2) >> 1; //u(5)
+				break;
+			case 4:
+				headers->sps.nalinitialCpbRemovalDelayLengthMinus1 = ((spsHeader[offset] & FINITIALCPBREMOVALDELAYLENGTHMINUS14H) << 1 ) | ((spsHeader[offset] & FINITIALCPBREMOVALDELAYLENGTHMINUS14L) >> 7); //u(5)
+				offset++;
+				headers->sps.nalcpbRemovalDelayLengthMinus1 = ((spsHeader[offset] & FCPBREMOVALDELAYLENGTHMINUS11) >> 2); //u(5)
+				headers->sps.naldpbOutputDelayLengthMinus1 = ((spsHeader[offset] & FDPBOUTPUTDEALYLENGTHMINUS16H) << 3) | ((spsHeader[offset+1] & FDPBOUTPUTDEALYLENGTHMINUS16L) >> 5); //u(5)
+				offset++;
+				headers->sps.naltimeOffsetLength = (spsHeader[offset] & FTIMEOFFSETLENGTH3); //u(5)
+				break;
+			case 5:
+				headers->sps.nalinitialCpbRemovalDelayLengthMinus1 = ((spsHeader[offset] & FINITIALCPBREMOVALDELAYLENGTHMINUS15H) << 2 ) | ((spsHeader[offset] & FINITIALCPBREMOVALDELAYLENGTHMINUS15L) >> 6); //u(5)
+				offset++;
+				headers->sps.nalcpbRemovalDelayLengthMinus1 = ((spsHeader[offset] & FCPBREMOVALDELAYLENGTHMINUS12) >> 1); //u(5)
+				headers->sps.naldpbOutputDelayLengthMinus1 = ((spsHeader[offset] & FDPBOUTPUTDEALYLENGTHMINUS17H) << 4) | ((spsHeader[offset+1] & FDPBOUTPUTDEALYLENGTHMINUS17L) >> 4); //u(5)
+				offset++;
+				headers->sps.naltimeOffsetLength = ((spsHeader[offset] & FTIMEOFFSETLENGT4H) << 1) | ((spsHeader[offset+1] & FTIMEOFFSETLENGTH4L) >> 7); //u(5)
+				offset++;
+				break;
+			case 6:
+				headers->sps.nalinitialCpbRemovalDelayLengthMinus1 = ((spsHeader[offset] & FINITIALCPBREMOVALDELAYLENGTHMINUS16H) << 3 ) | ((spsHeader[offset] & FINITIALCPBREMOVALDELAYLENGTHMINUS16L) >> 5); //u(5)
+				offset++;
+				headers->sps.nalcpbRemovalDelayLengthMinus1 = (spsHeader[offset] & FCPBREMOVALDELAYLENGTHMINUS13); //u(5)
+				offset++;
+				headers->sps.naldpbOutputDelayLengthMinus1 = ((spsHeader[offset] & FDPBOUTPUTDEALYLENGTHMINUS10) >> 3); //u(5)
+				headers->sps.naltimeOffsetLength = ((spsHeader[offset] & FTIMEOFFSETLENGT5H) << 2) | ((spsHeader[offset+1] & FTIMEOFFSETLENGTH5L) >> 6); //u(5)
+				offset++;
+				break;
+			case 7:
+				headers->sps.nalinitialCpbRemovalDelayLengthMinus1 = ((spsHeader[offset] & FINITIALCPBREMOVALDELAYLENGTHMINUS17H) << 4) | ((spsHeader[offset] & FINITIALCPBREMOVALDELAYLENGTHMINUS17L) >> 4); //u(5)
+				offset++;
+				headers->sps.nalcpbRemovalDelayLengthMinus1 = ((spsHeader[offset] & FCPBREMOVALDELAYLENGTHMINUS14H) << 1) | ((spsHeader[offset+1] & FCPBREMOVALDELAYLENGTHMINUS14L) >> 7); //u(5)
+				offset++;
+				headers->sps.naldpbOutputDelayLengthMinus1 = ((spsHeader[offset] & FDPBOUTPUTDEALYLENGTHMINUS11) >> 2); //u(5)
+				headers->sps.naltimeOffsetLength = ((spsHeader[offset] & FTIMEOFFSETLENGT6H) << 3) | ((spsHeader[offset+1] & FTIMEOFFSETLENGTH6L) >> 5); //u(5)
+				offset++;
+				break;
+			}
 		}
 		headers->sps.vclHrdParametersPresentFlag = (spsHeader[offset] & FVCLHRDPARAMETERSPRESENTLAG) >> 2;
 		if (headers->sps.vclHrdParametersPresentFlag) {
@@ -298,15 +397,6 @@ bool spsToRbsp(unsigned char* spsHeader, uint32_t spsLength, nalHeader* headers)
 			headers->sps.maxDecFrameBuffering = (spsHeader[offset] & FMAXEDCFRAMEBUFFERING) >> 1;
 			headers->sps.rbspStopBit = (spsHeader[offset] & FRBSPSTOPBIT) >> 1;
 		}
-
-
-
-
-
-
-
-
-
 	}
 
 	return true;
